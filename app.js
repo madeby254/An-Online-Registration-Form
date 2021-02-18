fde5a3d492bde565dcbc5982246fcad019959b09
madeby254 <mojitsu185@gmail.com>
2021-02-17 22:59:38 -0800

    cancelled

--- app.js
+++ app.js
@@ -13,3 +13,10 @@
 // guestList.unshift('Simba', 'Rhino');
 
 // console.log(guestList);
+
+const orderQueue = ['1XT567437', '1U7857317', '1I9222528'];
+
+const shipping = orderQueue.shift();
+const cancelled = orderQueue.pop();
+
+console.log(cancelled);
