094e75bef0fe6985e4a27cbe88fc5f4cbe060da3
madeby254 <mojitsu185@gmail.com>
2021-02-17 22:54:34 -0800

    quiz

--- app.js
+++ app.js
@@ -5,3 +5,11 @@
 // const players = ['Toni', 'Anwar', 'Mali', 'Carlos', 'Cormac', 'Sariah'];
 
 // console.log(players[5]);
+
+const guestList = ['Eleven', 'Dustin', 'Lucas', 'Will'];
+
+guestList.push('Paka', 'Sungura');
+
+guestList.unshift('Simba', 'Rhino');
+
+console.log(guestList);
